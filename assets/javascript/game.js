60e56a088bc23e75fb8907e9aed2b71c5787105f
Moon Child <kaitline.moon@gmail.com>
2018-08-21 16:47:21 -0400

    updating variables for game.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,20 @@
 $(document).ready(() => {
 
+var playerGuess = 0;
+var perfectNumber = 0;
+var playerWins = 0;
+var playerLosses = 0;
+var reachPerfectNumber = false;
+
+function initializeGame () {
+    playerGuess = "";
+    perfectNumber = "";
+    reachPerfectNumber = false;
+
+    $("#heart, #blue, #green, #red, #player-guess").empty();
+}
+
+
 $("#heart").on("click", function(){
     console.log(Math.floor(Math.random() * 13))
 });
