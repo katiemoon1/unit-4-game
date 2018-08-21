3a32781f73f57bd5c5db6d8af0dd13575d29129a
Moon Child <kaitline.moon@gmail.com>
2018-08-20 20:00:35 -0400

    Still can't link css, but successfully created a click event for crystal

---
+++ assets/javascript/game.js
@@ -0,0 +1,21 @@
+$(document).ready(() => {
+
+$("#heart").on("click", function(){
+    console.log(Math.floor(Math.random() * 13))
+});
+$("#blue").on("click", function(){
+    console.log(Math.floor(Math.random() * 13))
+});
+$("#green").on("click", function(){
+    console.log(Math.floor(Math.random() * 13))
+});
+$("#red").on("click", function(){
+    console.log(Math.floor(Math.random() * 13))
+});
+
+
+
+
+
+
+});
