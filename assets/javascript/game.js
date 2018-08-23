3b1d39ca8e7cf67667215b6ee599d9364b4386f1
Moon Child <kaitline.moon@gmail.com>
2018-08-23 18:31:06 -0400

    Updated JS to call the functions.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,36 +3,47 @@ $(document).ready(() => {
 var playerGuess = 0;
 var playerWins = 0;
 var playerLosses = 0;
-var heartCrystal =0;
+var heartCrystal = 0;
 var blueCrystal = 0;
 var greenCrystal = 0;
 var redCrystal = 0;
+var perfect = 0;
 
 // Function to clear values at the start of the game
 var gameStart = function () {
-    heartCrystal = "";
-    blueCrystal = "";
-    greenCrystal = "";
-    redCrystal = "";
     playerGuess = 0;
 
     $("#heart, #blue, #green, #red, #player-guess").empty();
-
 }
 
-var perfectNumber = function perfectNumber(min, max) {
-    min = Math.ceil(19);
-    max = Math.floor(120);
-    $("#perfect-number").attr(Math.floor(Math.random() * (120-19)) + 19);
-    $("#perfect-number").text(perfectNumber);
+// Function to set the values of the crystals
+var setCrystals = function () {
+    heartCrystal = Math.floor(Math.random() * 13);
+    blueCrystal = Math.floor(Math.random() * 13);
+    greenCrystal = Math.floor(Math.random() * 13);
+    redCrystal = Math.floor(Math.random() * 13);
 }
 
-// If/Else statement for keeping score, needs to be included in game function
-if (playerGuess === perfectNumber) {
-    $("#player-wins").append(playerWins++)
+// Function to create the desired value
+var perfectNumber = function perfectNumber() {
+    var min = Math.ceil(19);
+    var max = Math.floor(120);
+    var perfect = (Math.floor(Math.random() * (120-19)) + 19);
+    $("#perfect-number").append(perfect);
 }
-else if (playerGuess >= perfectNumber) {
-    $("#player-losses").append(playerLosses++)
-}
+
+// If/Else statement for keeping score
+var checking = function () {
+    if (playerGuess === perfectNumber) {
+        $("#player-wins").append(playerWins++)
+    }
+    else if (playerGuess >= perfectNumber) {
+        $("#player-losses").append(playerLosses++)
+    }};
+
+// Calling the functions
+gameStart();
+setCrystals();
+perfectNumber();
 
 });
